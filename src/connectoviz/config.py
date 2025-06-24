65c3d0b141691fc0b0d22c06e70867a7766e8766
Gal Kepler <galkepler@gmail.com>
2025-06-24 11:47:54 +0300

    removed irrelevant part from config

--- src/connectoviz/config.py
+++ src/connectoviz/config.py
@@ -11,13 +11,3 @@ PROJ_ROOT = Path(__file__).resolve().parent
 logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
 DATA_DIR = PROJ_ROOT / "data"
-
-# If tqdm is installed, configure loguru with tqdm.write
-# https://github.com/Delgan/loguru/issues/135
-try:
-    from tqdm import tqdm
-
-    logger.remove(0)
-    logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
-except ModuleNotFoundError:
-    pass
