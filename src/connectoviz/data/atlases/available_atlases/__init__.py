5183096864fbc250db0c90faa1b22defa304c533
Gal Kepler <galkepler@gmail.com>
2025-06-24 16:24:25 +0300

    removed dependency of kepost

--- src/connectoviz/data/atlases/available_atlases/__init__.py
+++ src/connectoviz/data/atlases/available_atlases/__init__.py
@@ -2,6 +2,6 @@
 This module contains the available atlases that can be used in the package.
 """
 
-from kepost.atlases.available_atlases.available_atlases import (  # noqa: F401
-    AVAILABLE_ATLASES,
+from connectoviz.data.atlases.available_atlases.available_atlases import (
+    AVAILABLE_ATLASES,  # noqa: F401
 )
